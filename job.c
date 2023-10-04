21f92960994e163ae219e3b39adb571d3503fd5b
roblee <roblee04@gmail.com>
2023-10-04 06:22:52 -0700

    job

--- job.c
+++ job.c
@@ -1,22 +1,16 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-struct node {
+typedef struct node {
     int arrival_time;
     int service_time;
     int priority;
     struct node* next;
-}
-
-int main()
-{
-
-    // call create job 10x
-    create_job();
-
-}
+} node;
 
 node* create_job() {
-    int seed = time(42069420); // always use this seed
+    int seed = time(NULL); // always use this seed
     srand(seed); // guarantee consistency when debugging
     int arrival_time = rand() % 100; // will return num between 0 and 99
     int service_time = (rand() % 11); // will return num between 0 and 10
@@ -24,11 +18,24 @@ node* create_job() {
     int priority = rand() % 5;
     if (priority = 0) priority += 1; // priority between 1 .. 4
 
-    struct node *new_job = (struct node*) malloc(sizeof(struct node));
+    node *new_job = (node*) malloc(sizeof(node));
     new_job->arrival_time = arrival_time;
     new_job->service_time = service_time;
     new_job->priority = priority;
     new_job->next = NULL;
 
-    return new_job
+    return new_job;
+}
+
+int main()
+{
+
+    // call create job 10x
+    node* job = create_job();
+    int a = job->arrival_time;
+    int b = job->service_time;
+    int c = job->priority;
+
+    printf("%d %d %d", a, b, c);
+
 }
